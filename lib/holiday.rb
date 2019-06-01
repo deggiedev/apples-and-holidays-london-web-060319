c89d0ef5136fdbeb5a2f25588defd519f8978a11
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 17:34:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
   end
-end
 end
 
 def all_holidays_with_bbq(holiday_hash)
